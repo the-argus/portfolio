0f35bfcae90e6bb724890b10bd123b1a8b00935e
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-04 14:58:51 -0600

    init flake and direnv

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "simple shell environment";
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs?rev=16f4e04658c2ab10114545af2f39db17d51bd1bd;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  } @ inputs: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    genSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgs = genSystems (system: import nixpkgs {inherit system;});
+
+    # not these though
+    stateVersion = "22.11";
+  in {
+    devShell = genSystems (system:
+      pkgs.${system}.mkShell {
+        packages = with pkgs.${system}; [
+          nodejs
+          nodePackages.live-server
+        ];
+      });
+  };
+}
