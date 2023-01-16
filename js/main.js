6715ef5a345cce5e657d4bf421af65010ba57717
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-16 15:22:24 -0600

    complete the generation of portfolio entries from entries.json

--- js/main.js
+++ js/main.js
@@ -16,20 +16,28 @@ const addEntries = async () => {
     const titleElement = document.createElement('h3')
     const imageElement = document.createElement('img')
     const descriptionElement = document.createElement('p')
+    const linkElement = document.createElement('a')
+    const linkTextElement = document.createElement('div')
 
     // modify their contents and classes
     entryElement.className = 'entry'
     titleElement.innerText = entry.name
     imageElement.src = entry.preview.src
     imageElement.alt = entry.preview.alt
-    const getFirstWord = /\b.*\b/di
+    linkTextElement.innerHTML = entry.link.text
+    linkElement.href = entry.link.url
+    linkElement.appendChild(linkTextElement)
+
+    // bold the first word of the description
+    const getFirstWord = /\b\w*\b/di
     const firstWord = entry.description.match(getFirstWord)[0]
-    descriptionElement.innerText = entry.description.replace(firstWord, `<strong>${firstWord}</strong>`)
+    descriptionElement.innerHTML = entry.description.replace(firstWord, `<strong>${firstWord}</strong>`)
 
     // add sub-elements to the parent div
     entryElement.appendChild(titleElement)
     entryElement.appendChild(imageElement)
     entryElement.appendChild(descriptionElement)
+    entryElement.appendChild(linkElement)
 
     // add to the list of entries
     entriesElement.appendChild(entryElement)
