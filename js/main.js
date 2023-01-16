fa006edfbc19378bc1764c924d449d7321668835
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-16 13:31:22 -0600

    load entries successfully, need to fix description formatting and add link button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const addEntries = async () => {
+  const entriesElement = document.querySelector('#entries')
   // fetch entries
-  const entries = await fetch('./content/entries.json')
+  const entriesResponse = await fetch('js/content/entries.json')
+
+  if (!entriesResponse.ok) {
+    throw new Error('Error fetching content JSON: ' + entriesResponse.status)
+  }
+
+  const entries = await entriesResponse.json()
 
   // convert to html elements
   for (const entry of entries) {
@@ -15,12 +22,20 @@ const addEntries = async () => {
     titleElement.innerText = entry.name
     imageElement.src = entry.preview.src
     imageElement.alt = entry.preview.alt
-    descriptionElement.innerText = entry.description
+    const getFirstWord = /\b.*\b/di
+    const firstWord = entry.description.match(getFirstWord)[0]
+    descriptionElement.innerText = entry.description.replace(firstWord, `<strong>${firstWord}</strong>`)
 
     // add sub-elements to the parent div
-    entryElement.addChild(titleElement)
+    entryElement.appendChild(titleElement)
+    entryElement.appendChild(imageElement)
+    entryElement.appendChild(descriptionElement)
+
+    // add to the list of entries
+    entriesElement.appendChild(entryElement)
   }
 }
 
-document.onload = () => {
+window.onload = () => {
+  addEntries()
 }
