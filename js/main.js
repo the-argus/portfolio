97551e5b68654669b254f54847bd3525cb3c7d3e
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-17 14:24:57 -0500

    make my own jank html parser

--- js/main.js
+++ js/main.js
@@ -31,8 +31,12 @@ const addEntries = async () => {
 
     // bold the first word of the description
     const getFirstWord = /\b\w*\b/di
+    const firstWordContainerElement = document.createElement('strong')
     const firstWord = entry.description.match(getFirstWord)[0]
-    descriptionElement.innerHTML = entry.description.replace(firstWord, `<strong>${firstWord}</strong>`)
+    firstWordContainerElement.appendChild(document.createTextNode(firstWord))
+    const descriptionPastFirstWord = document.createTextNode(entry.description.replace(firstWord, ''))
+    descriptionElement.appendChild(firstWordContainerElement)
+    descriptionElement.appendChild(descriptionPastFirstWord)
 
     // add sub-elements to the parent div
     entryElement.appendChild(titleElement)
@@ -43,6 +47,20 @@ const addEntries = async () => {
     // add to the list of entries
     entriesElement.appendChild(entryElement)
   }
+}
+
+function makeTextNodeFromText (text) {
+  const getLinks = /<a href="([^"]*)">([^<]*)<\/a>/g
+  const links = text.match(getLinks)
+  let modifiedText = text
+  // remove all links
+  for (let i = 0; i < links.length; i += 3) {
+    modifiedText = modifiedText.replace(links[i], '__REPLACEME__')
+  }
+  // split between links
+  const textSections = modifiedText.split('__REPLACEME__')
+
+  // construct the final text by applying each link as a node
 }
 
 window.onload = () => {
