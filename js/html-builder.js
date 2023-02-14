a59fb6ec754c68f9f98f95eadeb83746a221e1b0
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-13 22:50:34 -0500

    refactor html builder to module

---
+++ js/html-builder.js
@@ -0,0 +1,45 @@
+export function buildEntryHTML (entry) {
+  // create all the elements needed
+  const topContainer = document.createElement('div')
+  const bottomContainer = document.createElement('div')
+  const entryElement = document.createElement('div')
+  const titleElement = document.createElement('h3')
+  const imageElement = document.createElement('img')
+  const descriptionElement = document.createElement('p')
+  const linkElement = document.createElement('a')
+  const linkTextElement = document.createElement('div')
+
+  // modify their contents and classes
+  entryElement.className = 'entry'
+  titleElement.innerText = entry.name
+  imageElement.src = entry.preview.src
+  imageElement.alt = entry.preview.alt
+  linkTextElement.innerHTML = entry.link.text
+  linkElement.href = entry.link.url
+  linkElement.appendChild(linkTextElement)
+  linkElement.className = 'button'
+
+  // bold the first word of the description
+  const getFirstWord = /\b\w*\b/di
+  const firstWordContainerElement = document.createElement('strong')
+  const firstWord = entry.description.match(getFirstWord)[0]
+  firstWordContainerElement.appendChild(document.createTextNode(firstWord))
+
+  const markdownParser = new commonmark.Parser()
+  const markdownRenderer = new commonmark.HtmlRenderer({ safe: true })
+  descriptionElement.innerHTML = markdownRenderer.render(
+    markdownParser.parse(
+      entry.description.replace(firstWord, `**${firstWord}**`)
+    )
+  )
+
+  // add sub-elements to the parent div
+  topContainer.appendChild(titleElement)
+  topContainer.appendChild(imageElement)
+  bottomContainer.appendChild(descriptionElement)
+  bottomContainer.appendChild(linkElement)
+  entryElement.appendChild(topContainer)
+  entryElement.appendChild(bottomContainer)
+
+  return entryElement
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { buildEntryHTML } from './html-builder.js'
+
 const addEntries = async () => {
   const entriesElement = document.querySelector('#entries')
   // fetch entries
@@ -12,47 +14,7 @@ const addEntries = async () => {
   // convert to html elements
   for (const entryName in entriesInfo.entries) {
     const entry = entriesInfo.entries[entryName]
-    // create all the elements needed
-    const topContainer = document.createElement('div')
-    const bottomContainer = document.createElement('div')
-    const entryElement = document.createElement('div')
-    const titleElement = document.createElement('h3')
-    const imageElement = document.createElement('img')
-    const descriptionElement = document.createElement('p')
-    const linkElement = document.createElement('a')
-    const linkTextElement = document.createElement('div')
-
-    // modify their contents and classes
-    entryElement.className = 'entry'
-    titleElement.innerText = entry.name
-    imageElement.src = entry.preview.src
-    imageElement.alt = entry.preview.alt
-    linkTextElement.innerHTML = entry.link.text
-    linkElement.href = entry.link.url
-    linkElement.appendChild(linkTextElement)
-    linkElement.className = 'button'
-
-    // bold the first word of the description
-    const getFirstWord = /\b\w*\b/di
-    const firstWordContainerElement = document.createElement('strong')
-    const firstWord = entry.description.match(getFirstWord)[0]
-    firstWordContainerElement.appendChild(document.createTextNode(firstWord))
-
-    const markdownParser = new commonmark.Parser()
-    const markdownRenderer = new commonmark.HtmlRenderer({ safe: true })
-    descriptionElement.innerHTML = markdownRenderer.render(
-      markdownParser.parse(
-        entry.description.replace(firstWord, `**${firstWord}**`)
-      )
-    )
-
-    // add sub-elements to the parent div
-    topContainer.appendChild(titleElement)
-    topContainer.appendChild(imageElement)
-    bottomContainer.appendChild(descriptionElement)
-    bottomContainer.appendChild(linkElement)
-    entryElement.appendChild(topContainer)
-    entryElement.appendChild(bottomContainer)
+    const entryElement = buildEntryHTML(entry)
 
     // add to the list of entries
     entriesElement.appendChild(entryElement)
